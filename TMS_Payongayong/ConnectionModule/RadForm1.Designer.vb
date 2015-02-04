1a3523c2f74cb70ff5206aef686fa9d59aa39505
ryanpalado <jakepalado@yahoo.com>
2015-02-04 11:16:06 +0800

    New Added Form

---
+++ TMS_Payongayong/ConnectionModule/RadForm1.Designer.vb
@@ -0,0 +1,210 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class RadForm1
+    Inherits Telerik.WinControls.UI.RadForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtse = New Telerik.WinControls.UI.RadTextBox()
+        Me.txtu = New Telerik.WinControls.UI.RadTextBox()
+        Me.txtp = New Telerik.WinControls.UI.RadTextBox()
+        Me.txtpo = New Telerik.WinControls.UI.RadTextBox()
+        Me.txtdbase = New Telerik.WinControls.UI.RadTextBox()
+        Me.RadLabel1 = New Telerik.WinControls.UI.RadLabel()
+        Me.RadLabel2 = New Telerik.WinControls.UI.RadLabel()
+        Me.RadLabel3 = New Telerik.WinControls.UI.RadLabel()
+        Me.RadLabel4 = New Telerik.WinControls.UI.RadLabel()
+        Me.RadLabel5 = New Telerik.WinControls.UI.RadLabel()
+        Me.RadButton1 = New Telerik.WinControls.UI.RadButton()
+        Me.RadButton2 = New Telerik.WinControls.UI.RadButton()
+        Me.RadButton3 = New Telerik.WinControls.UI.RadButton()
+        CType(Me.txtse, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.txtu, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.txtp, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.txtpo, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.txtdbase, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadLabel2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadLabel3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadLabel4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadLabel5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadButton1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadButton2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadButton3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtse
+        '
+        Me.txtse.Location = New System.Drawing.Point(119, 23)
+        Me.txtse.Name = "txtse"
+        Me.txtse.Size = New System.Drawing.Size(220, 20)
+        Me.txtse.TabIndex = 0
+        '
+        'txtu
+        '
+        Me.txtu.Location = New System.Drawing.Point(119, 61)
+        Me.txtu.Name = "txtu"
+        Me.txtu.Size = New System.Drawing.Size(220, 20)
+        Me.txtu.TabIndex = 1
+        '
+        'txtp
+        '
+        Me.txtp.Location = New System.Drawing.Point(119, 102)
+        Me.txtp.Name = "txtp"
+        Me.txtp.Size = New System.Drawing.Size(220, 20)
+        Me.txtp.TabIndex = 1
+        '
+        'txtpo
+        '
+        Me.txtpo.Location = New System.Drawing.Point(119, 138)
+        Me.txtpo.Name = "txtpo"
+        Me.txtpo.Size = New System.Drawing.Size(220, 20)
+        Me.txtpo.TabIndex = 1
+        '
+        'txtdbase
+        '
+        Me.txtdbase.Location = New System.Drawing.Point(119, 178)
+        Me.txtdbase.Name = "txtdbase"
+        Me.txtdbase.Size = New System.Drawing.Size(220, 20)
+        Me.txtdbase.TabIndex = 1
+        '
+        'RadLabel1
+        '
+        Me.RadLabel1.Location = New System.Drawing.Point(36, 24)
+        Me.RadLabel1.Name = "RadLabel1"
+        Me.RadLabel1.Size = New System.Drawing.Size(36, 18)
+        Me.RadLabel1.TabIndex = 2
+        Me.RadLabel1.Text = "server"
+        '
+        'RadLabel2
+        '
+        Me.RadLabel2.Location = New System.Drawing.Point(36, 63)
+        Me.RadLabel2.Name = "RadLabel2"
+        Me.RadLabel2.Size = New System.Drawing.Size(27, 18)
+        Me.RadLabel2.TabIndex = 3
+        Me.RadLabel2.Text = "user"
+        '
+        'RadLabel3
+        '
+        Me.RadLabel3.Location = New System.Drawing.Point(36, 104)
+        Me.RadLabel3.Name = "RadLabel3"
+        Me.RadLabel3.Size = New System.Drawing.Size(54, 18)
+        Me.RadLabel3.TabIndex = 3
+        Me.RadLabel3.Text = "password"
+        '
+        'RadLabel4
+        '
+        Me.RadLabel4.Location = New System.Drawing.Point(36, 140)
+        Me.RadLabel4.Name = "RadLabel4"
+        Me.RadLabel4.Size = New System.Drawing.Size(27, 18)
+        Me.RadLabel4.TabIndex = 4
+        Me.RadLabel4.Text = "port"
+        '
+        'RadLabel5
+        '
+        Me.RadLabel5.Location = New System.Drawing.Point(36, 180)
+        Me.RadLabel5.Name = "RadLabel5"
+        Me.RadLabel5.Size = New System.Drawing.Size(46, 18)
+        Me.RadLabel5.TabIndex = 5
+        Me.RadLabel5.Text = "databse"
+        '
+        'RadButton1
+        '
+        Me.RadButton1.Enabled = False
+        Me.RadButton1.Location = New System.Drawing.Point(241, 238)
+        Me.RadButton1.Name = "RadButton1"
+        Me.RadButton1.Size = New System.Drawing.Size(110, 24)
+        Me.RadButton1.TabIndex = 6
+        Me.RadButton1.Text = "save"
+        '
+        'RadButton2
+        '
+        Me.RadButton2.Location = New System.Drawing.Point(107, 238)
+        Me.RadButton2.Name = "RadButton2"
+        Me.RadButton2.Size = New System.Drawing.Size(110, 24)
+        Me.RadButton2.TabIndex = 7
+        Me.RadButton2.Text = "test"
+        '
+        'RadButton3
+        '
+        Me.RadButton3.Location = New System.Drawing.Point(154, 208)
+        Me.RadButton3.Name = "RadButton3"
+        Me.RadButton3.Size = New System.Drawing.Size(110, 24)
+        Me.RadButton3.TabIndex = 8
+        Me.RadButton3.Text = "Reset"
+        '
+        'RadForm1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(409, 292)
+        Me.Controls.Add(Me.RadButton3)
+        Me.Controls.Add(Me.RadButton2)
+        Me.Controls.Add(Me.RadButton1)
+        Me.Controls.Add(Me.RadLabel5)
+        Me.Controls.Add(Me.RadLabel4)
+        Me.Controls.Add(Me.RadLabel2)
+        Me.Controls.Add(Me.RadLabel3)
+        Me.Controls.Add(Me.RadLabel1)
+        Me.Controls.Add(Me.txtpo)
+        Me.Controls.Add(Me.txtp)
+        Me.Controls.Add(Me.txtu)
+        Me.Controls.Add(Me.txtdbase)
+        Me.Controls.Add(Me.txtse)
+        Me.Name = "RadForm1"
+        '
+        '
+        '
+        Me.RootElement.ApplyShapeToControl = True
+        Me.Text = "RadForm1"
+        CType(Me.txtse, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.txtu, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.txtp, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.txtpo, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.txtdbase, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadLabel1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadLabel2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadLabel3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadLabel4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadLabel5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadButton1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadButton2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadButton3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtse As Telerik.WinControls.UI.RadTextBox
+    Friend WithEvents txtu As Telerik.WinControls.UI.RadTextBox
+    Friend WithEvents txtp As Telerik.WinControls.UI.RadTextBox
+    Friend WithEvents txtpo As Telerik.WinControls.UI.RadTextBox
+    Friend WithEvents txtdbase As Telerik.WinControls.UI.RadTextBox
+    Friend WithEvents RadLabel1 As Telerik.WinControls.UI.RadLabel
+    Friend WithEvents RadLabel2 As Telerik.WinControls.UI.RadLabel
+    Friend WithEvents RadLabel3 As Telerik.WinControls.UI.RadLabel
+    Friend WithEvents RadLabel4 As Telerik.WinControls.UI.RadLabel
+    Friend WithEvents RadLabel5 As Telerik.WinControls.UI.RadLabel
+    Friend WithEvents RadButton1 As Telerik.WinControls.UI.RadButton
+    Friend WithEvents RadButton2 As Telerik.WinControls.UI.RadButton
+    Friend WithEvents RadButton3 As Telerik.WinControls.UI.RadButton
+End Class
+
---
+++ TMS_Payongayong/ConnectionModule/RadForm1.vb
@@ -0,0 +1,60 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class RadForm1
+
+
+    Private Sub RadButton2_Click(sender As Object, e As EventArgs) Handles RadButton2.Click
+         Dim con As New MySqlConnection("server=" & txtse.Text & ";uid=" & txtu.Text & ";pwd=" & txtp.Text & ";port=" & txtpo.Text & ";database=" & txtdbase.Text & ";")
+
+
+        Try
+            con.Close()
+            con.Open()
+            MsgBox("Connected")
+            RadButton1.Enabled = True
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            RadButton1.Enabled = False
+        End Try
+
+    End Sub
+
+    Private Sub RadButton1_Click(sender As Object, e As EventArgs) Handles RadButton1.Click
+        With sets
+            .serverko = txtse.Text
+            .userko = txtu.Text
+            .portko = txtpo.Text
+            .passko = txtp.Text
+            .databaseko = txtdbase.Text
+            .Save()
+
+        End With
+        MsgBox("Saved")
+
+
+    End Sub
+
+    Private Sub RadButton3_Click(sender As Object, e As EventArgs) Handles RadButton3.Click
+        sets.Reset()
+    End Sub
+
+    Private Sub txtse_TextChanged(sender As Object, e As EventArgs) Handles txtse.TextChanged
+        RadButton1.Enabled = False
+    End Sub
+
+    Private Sub txtu_TextChanged(sender As Object, e As EventArgs) Handles txtu.TextChanged
+        RadButton1.Enabled = False
+    End Sub
+
+    Private Sub txtp_TextChanged(sender As Object, e As EventArgs) Handles txtp.TextChanged
+        RadButton1.Enabled = False
+    End Sub
+
+    Private Sub txtpo_TextChanged(sender As Object, e As EventArgs) Handles txtpo.TextChanged
+        RadButton1.Enabled = False
+    End Sub
+
+    Private Sub txtdbase_TextChanged(sender As Object, e As EventArgs) Handles txtdbase.TextChanged
+        RadButton1.Enabled = False
+    End Sub
+End Class
